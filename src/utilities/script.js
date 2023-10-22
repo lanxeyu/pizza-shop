221becb2aad3b88b81739217b1850d8324ddc80e
lanxeyu <lanxeyu@gmail.com>
2023-10-22 23:13:13 +0100

    Removed unused code

---
+++ src/utilities/script.js
@@ -0,0 +1,9 @@
+
+
+function generateRandom8DigitNumber() {
+    const min = 10000000; 
+    const max = 99999999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export {generateRandom8DigitNumber}
