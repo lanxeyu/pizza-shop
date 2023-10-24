9afba7dae2dc9f81f9842c77cb80672d1f3ea805
lanxeyu <lanxeyu@gmail.com>
2023-10-24 14:13:44 +0100

    Formatting

--- src/utilities/worker.js
+++ src/utilities/worker.js
@@ -1,9 +1,6 @@
-console.log('Service worker loaded...');
-
 self.addEventListener('push', e => {
     const data = e.data.json();
-    console.log('Push received... ');
     self.registration.showNotification(data.title, {
-        body: 'Notified by Pluto'
+        body: "Notified by Pluto's Pizza"
     });
 });
